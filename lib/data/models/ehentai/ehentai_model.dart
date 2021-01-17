4c8bdedda016b4f10a0461ee91e13ad3de47df02
simonkimi <805757448@qq.com>
2021-01-17 22:53:26 +0800

    添加接口

--- lib/data/models/ehentai/ehentai_model.dart
+++ lib/data/models/ehentai/ehentai_model.dart
@@ -1,26 +1,6 @@
-import 'package:catpic/data/database/entity/website_entity.dart';
 import 'package:catpic/ui/pages/main_page/models/large_card_model.dart';
 import 'package:catpic/ui/pages/main_page/models/simple_card_model.dart';
-
-import '../website_model.dart';
-
-class EHentaiModel extends WebsiteModel {
-  EHentaiModel(WebsiteEntity websiteEntity) : super(websiteEntity);
+import 'package:catpic/ui/store/website_store.dart';
 
 
-  @override
-  List<LargeCardModel> toLargeCardModel() {
 
-  }
-
-  @override
-  List<SimpleCardModel> toSimpleCardModel() {
-
-  }
-
-  @override
-  List getNextPage() {
-    // TODO: implement getNextPage
-    throw UnimplementedError();
-  }
-}
--- lib/ui/store/website_store.dart
+++ lib/ui/store/website_store.dart
@@ -1,18 +1,10 @@
 import 'package:catpic/data/database/entity/website_entity.dart';
 import 'package:catpic/ui/pages/main_page/models/large_card_model.dart';
 import 'package:catpic/ui/pages/main_page/models/simple_card_model.dart';
+import 'package:mobx/mobx.dart';
 
-abstract class WebsiteModel<T> {
-  final WebsiteEntity websiteEntity;
-  int pageIndex;
-
-  WebsiteModel(this.websiteEntity);
-
-
-
-
+abstract class WebsiteStore<T> {
+  List<LargeCardModel> get largeCardList;
+  List<SimpleCardModel> get simpleCardList;
   List<T> getNextPage();
-  List<LargeCardModel> toLargeCardModel();
-  List<SimpleCardModel> toSimpleCardModel();
-
 }
