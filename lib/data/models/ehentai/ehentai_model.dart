34768b308b91863b9b65edb9eb40a036a6eae913
simonkimi <805757448@qq.com>
2021-01-18 22:25:50 +0800

    参照以前项目构建下逻辑

--- lib/data/models/ehentai/ehentai_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:catpic/ui/pages/main_page/models/large_card_model.dart';
-import 'package:catpic/ui/pages/main_page/models/simple_card_model.dart';
-import 'package:catpic/ui/store/website_store.dart';
-
-
-
--- lib/ui/store/website_store.dart
+++ lib/ui/store/website_store.dart
@@ -1,10 +1,10 @@
-import 'package:catpic/data/database/entity/website_entity.dart';
+
 import 'package:catpic/ui/pages/main_page/models/large_card_model.dart';
 import 'package:catpic/ui/pages/main_page/models/simple_card_model.dart';
-import 'package:mobx/mobx.dart';
 
-abstract class WebsiteStore<T> {
+abstract class WebsiteStore {
   List<LargeCardModel> get largeCardList;
   List<SimpleCardModel> get simpleCardList;
-  List<T> getNextPage();
+  void loadNextPage();
+  void loadSpecifyPage(int page);
 }
