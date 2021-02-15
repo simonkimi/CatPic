5d06e1ebc32546c26f8dbbf97a683c76ad1956e9
simonkimi <805757448@qq.com>
2021-02-15 22:42:13 +0800

    去除无用代码

--- lib/ui/pages/main_page/main_page.dart
+++ lib/ui/pages/main_page/main_page.dart
@@ -1,9 +1,5 @@
-import 'package:catpic/data/database/database_helper.dart';
-import 'package:catpic/ui/components/search_bar.dart';
 import 'package:catpic/ui/fragment/drawer/main_drawer.dart';
-import 'package:catpic/ui/store/main/main_store.dart';
 import 'package:flutter/material.dart';
-import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class MainPage extends StatefulWidget {
   static String routeName = 'MainPage';
@@ -13,15 +9,9 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  String data;
-  List<SearchSuggestions> suggestion;
-  final FloatingSearchBarController _controller = FloatingSearchBarController();
-
   @override
   void initState() {
     super.initState();
-    data = '';
-    suggestion = [];
   }
 
   @override
@@ -29,62 +19,7 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       drawer: MainDrawer(),
       body: SafeArea(
-        child: Stack(
-          children: [
-            Container(),
-            FloatingSearchBar(
-              controller: _controller,
-              builder: _builder,
-              debounceDelay: const Duration(milliseconds: 100),
-              scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-              transitionDuration: const Duration(milliseconds: 300),
-              transitionCurve: Curves.easeInOut,
-              physics: const BouncingScrollPhysics(),
-              axisAlignment: 0,
-              openAxisAlignment: 0.0,
-              maxWidth: 600,
-              transition: CircularFloatingSearchBarTransition(),
-              onQueryChanged: _queryTag,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<void> _queryTag(String tags) async {
-    if (tags.trim().isEmpty) {
-      // TODO 历史记录
-
-    } else {
-      final lastTag = tags.trim().split(' ').where((e) => e.isNotEmpty).last;
-      final dao = DatabaseHelper().tagDao;
-      final list = await dao.getStart(mainStore.websiteEntity.id, '$lastTag%');
-      final suggestions = list.map((e) => SearchSuggestions(e.tag));
-      setState(() {
-        suggestion = suggestions.toList();
-      });
-    }
-  }
-
-  Widget _builder(BuildContext context, Animation<double> transition) {
-    return Material(
-      color: Colors.white,
-      elevation: 4.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      animationDuration: Duration(seconds: 5),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: suggestion.map((e) {
-          return ListTile(
-            title: Text(e.title),
-            onTap: () {
-              _controller.query = e.title;
-            },
-          );
-        }).toList(),
+        child: Container(),
       ),
     );
   }
